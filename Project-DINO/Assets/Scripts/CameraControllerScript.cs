8607e4f0a20eff4d5d365ef234fb6563af95815c
xXGreeXx <lrhude.elijah@gmail.com>
2018-06-11 17:09:26 -0400

    Worked on some base models

--- Project-DINO/Assets/Scripts/CameraControllerScript.cs
+++ Project-DINO/Assets/Scripts/CameraControllerScript.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraControllerScript : MonoBehaviour {
+public class CameraControllerScript : MonoBehaviour
+{
+    Vector3 target = new Vector3(-638, 0, -244);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //start
+    void Start()
+    {
+        transform.LookAt(target);
+    }
+
+    //update
+    void Update()
+    {
+
+        //scroll camera in and out
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && Camera.main.fieldOfView > 5)
+        {
+            Camera.main.fieldOfView -= 5;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f && Camera.main.fieldOfView < 95)
+        {
+            Camera.main.fieldOfView += 5;
+        }
+
+        //move camera
+        if (Input.GetMouseButton(0))
+        {
+            transform.LookAt(target);
+            transform.RotateAround(target, Vector3.up, Input.GetAxis("Mouse X") * 2);
+        }
+    }
 }
