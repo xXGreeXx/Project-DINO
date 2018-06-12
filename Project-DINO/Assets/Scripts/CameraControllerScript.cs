b9a02cf03a3fc237367d23679a86ebaa12d3e781
xXGreeXx <lrhude.elijah@gmail.com>
2018-06-12 00:59:51 -0400

    Worked on map

--- Project-DINO/Assets/Scripts/CameraControllerScript.cs
+++ Project-DINO/Assets/Scripts/CameraControllerScript.cs
@@ -2,7 +2,9 @@
 
 public class CameraControllerScript : MonoBehaviour
 {
-    Vector3 target = new Vector3(-638, 0, -244);
+    Vector3 target = new Vector3(-9797, 2458, -4142);
+    float xVel = 0;
+    float yVel = 0;
 
     //start
     void Start()
@@ -13,7 +15,6 @@ public class CameraControllerScript : MonoBehaviour
     //update
     void Update()
     {
-
         //scroll camera in and out
         if (Input.GetAxis("Mouse ScrollWheel") > 0f && Camera.main.fieldOfView > 5)
         {
@@ -30,5 +31,18 @@ public class CameraControllerScript : MonoBehaviour
             transform.LookAt(target);
             transform.RotateAround(target, Vector3.up, Input.GetAxis("Mouse X") * 2);
         }
+
+        //change target point
+        if (Input.GetKey(KeyCode.W)) yVel = -15;
+        else if (Input.GetKey(KeyCode.S)) yVel = 15;
+        else yVel = 0;
+
+        if (Input.GetKey(KeyCode.A)) xVel = 15;
+        else if (Input.GetKey(KeyCode.D)) xVel = -15;
+        else xVel = 0;
+
+
+        this.transform.position += Vector3.Scale(new Vector3(xVel, 0, yVel), Vector3.one);
+        target += Vector3.Scale(new Vector3(xVel, 0, yVel), Vector3.one);
     }
 }
