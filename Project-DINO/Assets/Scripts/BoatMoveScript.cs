9ee0c62bbfeeb05f9c5b03ab0dd907833f6a689d
xXGreeXx <lrhude.elijah@gmail.com>
2018-06-12 15:32:24 -0400

    Added hotel and visitor complex, boat now moves +  basic other changes

---
+++ Project-DINO/Assets/Scripts/BoatMoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMoveScript : MonoBehaviour {
+
+    public static bool bringInBoat = true;
+    float speed = 500;
+
+	void Start ()
+    {
+	}
+
+    void Update()
+    {
+        if (bringInBoat && speed > 0)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+
+            if (transform.position.x > -14670)
+            {
+                speed -= 1.25F;
+            }
+        }
+    }
+}
