48325dcaedf0dddf8edea926fc6bf0f10d18d4c7
xXGreeXx <lrhude.elijah@gmail.com>
2018-06-12 18:12:08 -0400

    Worked some more

---
+++ Project-DINO/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+    GameObject mapPane;
+
+    public void Start()
+    {
+        mapPane = GameObject.Find("MapPane");
+        mapPane.SetActive(false);
+    }
+
+    public void ButtonPressed(string button)
+    {
+        Debug.Log("True");
+        if (button.Equals("map"))
+        {
+            mapPane.SetActive(!mapPane.activeInHierarchy);
+        }
+    }
+
+}
