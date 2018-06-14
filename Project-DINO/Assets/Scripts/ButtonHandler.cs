194a1e0234d744b936f633f7c3d6842558fcfdf9
xXGreeXx <lrhude.elijah@gmail.com>
2018-06-14 00:46:18 -0400

    Last commit

--- Project-DINO/Assets/Scripts/ButtonHandler.cs
+++ Project-DINO/Assets/Scripts/ButtonHandler.cs
@@ -6,6 +6,7 @@ public class ButtonHandler : MonoBehaviour {
     public static GameObject optionsPane;
     public static GameObject buildPane;
     public static GameObject buildingDataPane;
+    public static GameObject environmentPane;
     public static GameObject digSitePane;
 
     public void Start()
@@ -14,12 +15,15 @@ public class ButtonHandler : MonoBehaviour {
         optionsPane = GameObject.Find("OptionsPane");
         buildPane = GameObject.Find("BuildPane");
         buildingDataPane = GameObject.Find("BuildingDataPane");
+        environmentPane = GameObject.Find("EnvironmentPane");
         digSitePane = GameObject.Find("SiteDataPane");
         mapPane.SetActive(false);
         optionsPane.SetActive(false);
         buildPane.SetActive(false);
         buildingDataPane.SetActive(false);
+        environmentPane.SetActive(false);
         digSitePane.SetActive(false);
+
     }
 
     public void ButtonPressed(string button)
@@ -33,6 +37,10 @@ public class ButtonHandler : MonoBehaviour {
         {
             buildPane.SetActive(!buildPane.activeInHierarchy);
             mapPane.SetActive(false);
+        }
+        if (button.Equals("environment"))
+        {
+            environmentPane.SetActive(!environmentPane.activeInHierarchy);
         }
         if (button.Equals("options"))
         {
@@ -60,6 +68,16 @@ public class ButtonHandler : MonoBehaviour {
             o.AddComponent<BuildingPlacementHandler>();
             buildPane.SetActive(false);
         }
+
+        if (button.Equals("upterrain"))
+        {
+            MainGameHandler.selectedMouseTool = "up";
+        }
+        if (button.Equals("downterrain"))
+        {
+            MainGameHandler.selectedMouseTool = "down";
+        }
+
         if (button.Equals("Site1"))
         {
             digSitePane.SetActive(true);
@@ -94,6 +112,69 @@ public class ButtonHandler : MonoBehaviour {
             digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #5";
             digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Tanzania, Africa";
             digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$1850";
+        }
+        if (button.Equals("Site6"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #6";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Morroco, Africa";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2350";
+        }
+        if (button.Equals("Site7"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #7";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Argentina, South America";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site8"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #8";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Alberta, Canada";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site9"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #9";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Kazakhstan, Asia";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site10"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #10";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Brazil, South America";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site11"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #11";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Novia Scotia, Canada";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site12"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #12";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Germany, Europe";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site13"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #13";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Ukraine, Europe";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
+        }
+        if (button.Equals("Site14"))
+        {
+            digSitePane.SetActive(true);
+            digSitePane.transform.Find("Title").GetComponent<UnityEngine.UI.Text>().text = "Site #14";
+            digSitePane.transform.Find("Location").GetComponent<UnityEngine.UI.Text>().text = "Ukraine, Europe";
+            digSitePane.transform.Find("Cost").GetComponent<UnityEngine.UI.Text>().text = "$2650";
         }
     }
 
---
+++ Project-DINO/Assets/Scripts/MainGameHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameHandler : MonoBehaviour {
+
+    public static string selectedMouseTool = string.Empty;
+    Terrain t;
+    float[,] originalHeights;
+
+    // Use this for initialization
+    void Start ()
+    {
+        t = GameObject.Find("Terrain").GetComponent<Terrain>();
+        originalHeights = t.terrainData.GetHeights(0, 0, t.terrainData.heightmapWidth, t.terrainData.heightmapHeight);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //handle mouse tools
+        if (selectedMouseTool.Equals("up") && Input.GetMouseButton(0))
+        {
+            Vector3 pos = Vector3.zero;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                pos = hit.point;
+            }
+
+            float x = pos.x - t.transform.position.x;
+            float y = pos.z - t.transform.position.z;
+            Debug.Log(originalHeights.Length);
+            originalHeights[(int)x, (int)y] = 1F;
+
+            t.terrainData.SetHeights(0, 0, originalHeights);
+        }
+	}
+}
